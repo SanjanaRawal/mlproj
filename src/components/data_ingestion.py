a73682f6cc163031d810155ddce8f2d009ecfc22
SanjanaRawal <sanjana.rawal2006@gmail.com>
2026-03-08 17:10:43 +0530

    Model Training

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from src.exception import CustomException
 from src.logger import logging 
 from dataclasses import dataclass #used to create class variables
 from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 # Any input required in data ingestion component can be given here
@@ -43,6 +44,7 @@ if __name__ == "__main__" :
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()     
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_arr, test_arr = data_transformation.initiate_data_transformation(train_data, test_data)
     
-    
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
